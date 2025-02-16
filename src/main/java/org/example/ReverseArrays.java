80a87743a0cc1f38dc5ec33de7fb208d6298a167
hozdemir33 <hozdemir8233@gmail.com>
2025-02-16 00:04:31 -0800

    second commit

--- src/main/java/org/example/ReverseArrays.java
+++ src/main/java/org/example/ReverseArrays.java
@@ -10,9 +10,31 @@ public class ReverseArrays {
 
         //how to reverse arrays list from 1,2,3 to [ 3,2,1 ]
         int [] num= {1,2,3} ;
+
         System.out.println("This is the first output " + Arrays.toString(num)); // [1,2,3]
 
-        int [] n = new int[2] ;
+
+        int [] reversedArray = new int [num.length];
+
+        for(int i=0; i<num.length;i++)
+        {
+
+          reversedArray [i]= num[num.length-1-i] ;
+
+        }
+
+       // System.out.println(Arrays.toString(reversedArray));
+
+
+        //ArrayList----Collection.reverse()
+
+
+        ArrayList<Integer> list=new ArrayList<>(Arrays.asList(1,2,3));
+
+        Collections.reverse(list);
+
+        System.out.println("This is the reveresed form ::"+list);
+
 
     }
 }
