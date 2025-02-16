7124ba716616632ce0d8f770fddb1f4857b93648
hozdemir33 <hozdemir8233@gmail.com>
2025-02-15 23:54:15 -0800

    second commit

---
+++ src/main/java/org/example/ReverseArrays.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.*;
+import java.util.Arrays;
+
+public class ReverseArrays {
+
+    public static void main(String[] args) {
+
+
+        //how to reverse arrays list from 1,2,3 to [ 3,2,1 ]
+        int [] num= {1,2,3} ;
+        System.out.println("This is the first output " + Arrays.toString(num)); // [1,2,3]
+
+        int [] n = new int[2] ;
+
+    }
+}
